093be1cbb4c6c9a90027af0974700599a937d57a
Asako Kondo <kondoasako@gmail.com>
2021-07-05 06:20:49 +0900

    コンポーネントのルーティング

---
+++ src/views/Count.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Counter name="C1" :initCount="5" @emitUp="getEvent" />
+    <Counter name="C2" :initCount="10" @emitUp="getEvent" />
+    <p>
+      EventStack:
+      {{ stack }}
+    </p>
+  </div>
+</template>
+
+<script>
+import Counter from '@/components/Counter.vue'
+export default {
+  components: {
+    Counter
+  },
+  data() {
+    return {
+      stack: []
+    }
+  },
+  methods: {
+    getEvent(payload) {
+      this.stack.push(payload)
+    }
+  }
+}
+</script>
