b2c00a56f5837806763c4532d58d5354bbb5b839
Asako Kondo <kondoasako@gmail.com>
2021-07-06 22:32:19 +0900

    octcatの情報を取得

--- src/store/index.js
+++ src/store/index.js
@@ -5,15 +5,30 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    globalCount: 0
+    globalCount: 0,
+    profile: null,
   },
   mutations: {
     globalIncrement(state) {
       state.globalCount++
-    }
+    },
+    setGithubProfile(state, payload) {
+      state.profile = payload;
+    },
   },
   actions: {
+    async fetchGithubProfile(store, payload) {
+      if(store.state.profile !== null) return;
+
+      const response = await fetch(
+        `https://api.github.com/users/${payload.user_id}`
+      ).then((res) => {
+        return res.json();
+      });
+
+      store.commit("setGithubProfile", response);
+    },
   },
   modules: {
   }
-})
+});
